9f41334f58aa921c8a57c4cd79e6cbbac916476d
anabeatrizcruz <anabeatriz@musa.co>
2022-10-14 15:43:59 -0300

    fix: add props to components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,14 +5,16 @@ import { ReactNode } from "react";
 export interface ButtonProps {
   children: ReactNode;
   asChild?: boolean;
+  className: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className }: ButtonProps) {
   const Comp = asChild ? Slot : "button";
   return (
     <Comp
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-200 focus:ring-2 ring-white"
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-200 focus:ring-2 ring-white",
+        className
       )}
     >
       {children}
